80da9ff7635ff5e1fa5d29d82d6828e73b0860d2
Henrique Shodi Maeta <japa1996@hotmail.com>
2016-11-30 21:20:04 -0200

    FOASE

--- final/main/main.s
+++ final/main/main.s
@@ -2,9 +2,28 @@
                     .global loop
                     
 setup:
-              .EQU PORTB, 5
-              .EQU DDRB, 4
-              SBI 5, 5
+              .EQU DDRB, 0X04
+              .EQU PORTB, 0X05
+              sbi DDRB, 5
+              sbi DDRB, 4
               
+r24_init:
+              ldi r24, 255
+              ret
+
 loop:
-              sbi 5,5
+
+              RCALL r24_init
+              sbi PORTB, 4
+              sbi PORTB, 5
+              dec r24
+              cp r24, 0
+              breq turnoff
+              
+turnoff:
+              cbi PORTB, 4
+              cbi PORTB, 5
+              ldi r23, 255
+              dec r23
+              cpse r23, 0
+              
