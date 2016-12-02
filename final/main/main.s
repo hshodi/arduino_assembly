60edcd0f714134c379e9e0c263161469e6f28f7d
Henrique Shodi Maeta <japa1996@hotmail.com>
2016-12-02 01:49:54 -0200

    Dois leds oscilando

--- final/main/main.s
+++ final/main/main.s
@@ -1,21 +1,17 @@
               .global setup
               .global loop
-              
-              
+                    
 setup:
-              .EQU DDRB, 0x04
-              .EQU PORTB, 0x03
-              SBI DDRB, 5
-              SBI DDRB, 4
-              SBI DDRB, 3
+              .EQU DDRB, 0X04
+              .EQU PORTB, 0X03
+              sbi DDRB, 5
+              sbi DDRB, 4
+
+
+loop:         
+              sbi PORTB, 5
+              nop
+              SBI PORTB, 4              
+              nop
+              call loop
               
-loop:
-              SBI PORTB, 5
-              SLEEP
-              CBI PORTB, 5
-              SBI PORTB, 4
-              SLEEP
-              CBI PORTB, 4
-              SBI PORTB, 3
-              SLEEP
-              CBI PORTB, 3
